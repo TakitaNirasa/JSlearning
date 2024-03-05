637def4c042f19a30987630e39a781b7a53dcd99
TakitaNirasa <45029320+TakitaNirasa@users.noreply.github.com>
2024-03-05 14:22:15 +0300

    Создана ДБ

---
+++ 09/controller/user_controller.js
@@ -0,0 +1,20 @@
+class UserController {
+    async createUser(req, res) {
+        
+    }
+    async getUsers(req, res) {
+        
+    }
+    async getOneUser(req, res) {
+        
+    }
+    async updateUser(req, res) {
+        
+    }
+    async deleteUsers(req, res) {
+        
+    }
+
+}
+
+module.exports = new UserController()
